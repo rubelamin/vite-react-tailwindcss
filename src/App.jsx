86e1d0b5a64d3f1a8a2808c841ce156a8e3d3b0e
Rubel Amin <bagha.vai100@gmail.com>
2024-01-08 00:19:58 +0600

    push to the github

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+       <h1
+        className="text-3xl text-center text-red-700"
+       >Welcome to vite ract tailwindcss template project</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
